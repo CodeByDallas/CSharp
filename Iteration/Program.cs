756cd2172cb658504f99f787269300ed45213176
CodeByDallas <dallas.desmond1@outlook.com>
2024-06-17 10:31:29 +0100

    Add files via upload
    Iteration

---
+++ Iteration/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+Console.WriteLine("\n");
+
+
+// Without curly braces
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") 
+        names[i] = "Sammy";
+
+foreach (var name in names) 
+    Console.WriteLine(name);
