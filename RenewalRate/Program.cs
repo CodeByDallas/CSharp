ea314f452aef4b05a207d48cc0dc46fdb48044bd
CodeByDallas <dallas.desmond1@outlook.com>
2024-06-17 10:29:49 +0100

    Add files via upload
    Renewal Rate

---
+++ RenewalRate/Program.cs
@@ -0,0 +1,28 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine("Enter Log in name as (name)");
+string login = Console.ReadLine();
+
+if(login == "name")
+{
+    if(daysUntilExpiration > 1 && daysUntilExpiration < 5)
+    {
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days");
+        discountPercentage = 10;
+    }
+    if(daysUntilExpiration == 1)
+    {
+        Console.WriteLine($"Your subscription expires within {daysUntilExpiration} day");
+        discountPercentage = 20;
+    }
+    if(daysUntilExpiration == 0)
+    {
+        Console.WriteLine("Your subscription has expired");
+    }
+    if(daysUntilExpiration > 0)
+    {
+        Console.WriteLine($"Renew now and save {discountPercentage}%");
+    }
+}
